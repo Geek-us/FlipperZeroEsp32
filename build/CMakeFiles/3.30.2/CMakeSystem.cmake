0d32973217c47cd9ab8259419facad4440247396
Ghost <2643782046@qq.com>
2026-02-11 15:09:24 +0800

    生成compile_commands.json。

---
+++ build/CMakeFiles/3.30.2/CMakeSystem.cmake
@@ -0,0 +1,15 @@
+set(CMAKE_HOST_SYSTEM "Darwin-25.0.0")
+set(CMAKE_HOST_SYSTEM_NAME "Darwin")
+set(CMAKE_HOST_SYSTEM_VERSION "25.0.0")
+set(CMAKE_HOST_SYSTEM_PROCESSOR "arm64")
+
+include("/Users/Gray/espidf/esp-idf_5_5_2/v5.5.2/esp-idf/tools/cmake/toolchain-esp32c5.cmake")
+
+set(CMAKE_SYSTEM "Generic")
+set(CMAKE_SYSTEM_NAME "Generic")
+set(CMAKE_SYSTEM_VERSION "")
+set(CMAKE_SYSTEM_PROCESSOR "")
+
+set(CMAKE_CROSSCOMPILING "TRUE")
+
+set(CMAKE_SYSTEM_LOADED 1)
